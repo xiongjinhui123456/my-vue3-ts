69498ba686d0c156799617dc5956b15f155841f1
xiongjinhui <2650748689@qq.com>
2025-03-12 13:25:08 +0800

    fix: 解决el-message提示框显示位置不正确

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue';
 import './style.css'
 import router from './router';
 import { createPinia } from 'pinia';
+import "element-plus/theme-chalk/el-message.css";
+import "element-plus/theme-chalk/el-message-box.css"; 
 
 const app = createApp(App);
 const pinia = createPinia();
